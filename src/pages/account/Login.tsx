6c8f485a4aced1b5096c259b28529924cdc0fd43
Ritik Yaduvanshi <yadavritik467@gmail.com>
2024-09-02 20:14:58 +0530

    fix:remove login with google btn for a while

--- src/pages/account/Login.tsx
+++ src/pages/account/Login.tsx
@@ -40,10 +40,10 @@ const Login = () => {
 		else navigate("/login")
 	}, [accessToken])
 
-	const handleLogin = () => {
-		console.log("click")
-		window.location.href = "http://localhost:4500/auth/google" // Redirect to your backend for authentication
-	}
+	// const handleLogin = () => {
+	// 	console.log("click")
+	// 	window.location.href = "http://localhost:4500/auth/google" // Redirect to your backend for authentication
+	// }
 
 	return (
 		<div className="flex justify-center items-center h-[100vh] bg-gray-200">
@@ -70,13 +70,13 @@ const Login = () => {
 					>
 						Login
 					</button>
-					<button
+					{/* <button
 						onClick={handleLogin}
 						type="button"
 						className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 					>
 						Login With Google
-					</button>
+					</button> */}
 					<button
 						onClick={() => navigate("/signup")}
 						type="button"
